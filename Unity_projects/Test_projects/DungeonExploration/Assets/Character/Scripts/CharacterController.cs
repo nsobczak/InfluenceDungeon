6ec49111390e6ac5b27ae891ee9ddba018bd21b8
nsobczak <nicolas.sobczak@orange.fr>
2018-01-27 21:52:07 +0000

    - Test_project: add DungeonExploration project - Package: update dungeonCreation package

---
+++ Unity_projects/Test_projects/DungeonExploration/Assets/Character/Scripts/CharacterController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] private string FLOOR_TAG = "Floor";
+    [SerializeField] private float PLAYER_Y_OFFSET = 0.8f;
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            Debug.DrawLine(ray.origin, hit.point);
+            if (hit.transform.CompareTag(FLOOR_TAG) && Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("hit object is: " + hit.transform.name);
+                transform.position = new Vector3(hit.transform.position.x, PLAYER_Y_OFFSET, hit.transform.position.z);
+            }
+        }
+    }
+}
