0782b4c2dadb3bf78dfd733fd51d80e3fb7dd4c4
nsobczak <nicolas.sobczak@orange.fr>
2018-01-27 18:35:51 +0000

    - MainProject: add fight interface

---
+++ Unity_projects/Main_project/Assets/Menu/Scripts/ShowPanelsBattle.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowPanelsBattle : MonoBehaviour
+{
+    #region Parameters
+
+    public GameObject objectPanel;
+    public GameObject magicPanel; 
+
+    #endregion
+
+    //_________________________________________________
+
+    #region ShowFunctions
+
+    public void ShowObjectPanel()
+    {
+        objectPanel.SetActive(true);
+    }
+
+    public void ShowMagicPanel()
+    {
+        magicPanel.SetActive(true);
+    }
+
+    #endregion
+
+
+    //_________________________________________________
+
+    #region HideFunctions
+
+    public void HideObjectPanel()
+    {
+        objectPanel.SetActive(false);
+    }
+
+    public void HideMagicPanel()
+    {
+        magicPanel.SetActive(false);
+    }
+
+    #endregion
+}
