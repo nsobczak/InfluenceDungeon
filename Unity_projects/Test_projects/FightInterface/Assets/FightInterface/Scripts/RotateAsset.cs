1e18a4d4df4a42ae37a8d1f564ef7ac25c14f962
nsobczak <nicolas.sobczak@orange.fr>
2018-01-27 15:55:30 +0000

    - Test_projects: add FightInterface project - Package: add fight interface

---
+++ Unity_projects/Test_projects/FightInterface/Assets/FightInterface/Scripts/RotateAsset.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAsset : MonoBehaviour
+{
+    [SerializeField] private float RotationSpeed = 10f;
+
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * (RotationSpeed * Time.deltaTime));
+    }
+}
