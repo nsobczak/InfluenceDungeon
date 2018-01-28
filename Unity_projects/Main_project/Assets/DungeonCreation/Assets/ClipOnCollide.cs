64f978691640162afdb15fa7d419598b9fcbe8fe
Adnihilis <adnihilis@github.com>
2018-01-28 11:51:53 +0100

    Modification creation donjon
    Ajout prefab Cross Trigger, ajout du script de clip. Problème à résoudre : il faut pouvoir bloquer les éléments de crossTrigger pour ne pas pouvoir les bouger

---
+++ Unity_projects/Main_project/Assets/DungeonCreation/Assets/ClipOnCollide.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClipOnCollide : MonoBehaviour {
+	DragBricks script;
+	void Start() {
+		script = transform.parent.gameObject.GetComponent<DragBricks> ();
+	}
+	void OnTriggerStay(Collider other){
+		if(Input.GetMouseButtonUp(0))script.Clip (other.transform.parent.gameObject);
+	}
+}
