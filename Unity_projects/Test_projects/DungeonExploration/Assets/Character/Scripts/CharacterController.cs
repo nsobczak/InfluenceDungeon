6f54dbed577ebfec44e1c62035f2945bff04d4b4
nsobczak <nicolas.sobczak@orange.fr>
2018-01-28 00:00:19 +0000

    - DungeonExploration: handle event when trap tiles is encountered

--- Unity_projects/Test_projects/DungeonExploration/Assets/Character/Scripts/CharacterController.cs
+++ Unity_projects/Test_projects/DungeonExploration/Assets/Character/Scripts/CharacterController.cs
@@ -7,6 +7,9 @@ public class CharacterController : MonoBehaviour
     #region Attributes
 
     [SerializeField] private string FLOOR_TAG = "Floor";
+    [SerializeField] private string TRAP_SPIKES_TAG = "TrapSpikes";
+    [SerializeField] private string TRAP_HOLES_TAG = "TrapHoles";
+    [SerializeField] private string TRAP_OTHER_TAG = "TrapOther";
     [SerializeField] private float PLAYER_Y_OFFSET = 0.8f;
     [SerializeField] private GameObject floorPrefab;
     private List<GameObject> coloredTiles;
@@ -39,6 +42,30 @@ public class CharacterController : MonoBehaviour
     }
 
 
+    //check if tile contains a special tile
+    private void CheckTileEvent(GameObject tile)
+    {
+        if (tile.transform.childCount > 0)
+        {
+            //TODO: handle trap
+            Debug.Log("It's a trap !");
+            Transform childTile = tile.transform.GetChild(0);
+            if (childTile.CompareTag(TRAP_SPIKES_TAG))
+            {
+                Debug.Log("Trap is: " + TRAP_SPIKES_TAG);
+            }
+            else if (childTile.CompareTag(TRAP_HOLES_TAG))
+            {
+                Debug.Log("Trap is: " + TRAP_HOLES_TAG);
+            }
+            else //banana peel
+            {
+                Debug.Log("Trap is: " + TRAP_OTHER_TAG);
+            }
+        }
+    }
+
+
     private void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -51,15 +78,17 @@ public class CharacterController : MonoBehaviour
             {
                 hit.transform.GetComponent<MeshRenderer>().materials[0].color = Color.red;
                 coloredTiles.Add(hit.transform.gameObject);
-                
+
                 if (Input.GetMouseButtonDown(0))
                 {
-                    Debug.Log("hit object is: " + hit.transform.name);
+//                    Debug.Log("hit object is: " + hit.transform.name);
                     transform.position =
                         new Vector3(hit.transform.position.x, PLAYER_Y_OFFSET, hit.transform.position.z);
-                    
+
                     foreach (var tile in coloredTiles)
                         tile.transform.GetComponent<MeshRenderer>().materials[0].color = Color.white;
+
+                    CheckTileEvent(hit.transform.gameObject);
                 }
             }
         }
