604613340821ab6247d7d15036a49b2917c04e2e
PPJ <p.p-j@hotmail.fr>
2018-01-28 12:01:02 +0100

    explosion

--- Unity_projects/Main_project/Assets/DungeonCreation/Scripts/DragBricks.cs
+++ Unity_projects/Main_project/Assets/DungeonCreation/Scripts/DragBricks.cs
@@ -7,6 +7,8 @@ public class DragBricks : MonoBehaviour {
 	public float blocSize=5;
 	private Vector3 screenPoint;
 	private Vector3 offset;
+	public bool joinUp=true,joinDown=true,joinLeft=true,joinRight=true;
+	public float joinDistanceUp=10,joinDistanceDown=10,joinDistanceLeft=10,joinDistanceRight=10;
 
 	void OnMouseDown()
 	{
@@ -27,13 +29,13 @@ public class DragBricks : MonoBehaviour {
 		Vector3 uwd = transform.TransformDirection(new Vector3(0,1,0));
 		Vector3 dwd = transform.TransformDirection(new Vector3(0,-1,0));
 		RaycastHit hit;
-		if (Physics.Raycast (transform.position, fwd, out hit, 10)) {
+		if (Physics.Raycast (transform.position, fwd, out hit, joinDistanceRight)&&joinRight) {
 			transform.position = new Vector3 (hit.transform.position.x - blocSize, hit.transform.position.y, hit.transform.position.z);
-		} else if (Physics.Raycast (transform.position, bwd, out hit, 10)) {
+		} else if (Physics.Raycast (transform.position, bwd, out hit, joinDistanceLeft)&&joinLeft) {
 			transform.position = new Vector3 (hit.transform.position.x + blocSize, hit.transform.position.y, hit.transform.position.z);
-		}else if (Physics.Raycast (transform.position, uwd, out hit, 10)) {
+		}else if (Physics.Raycast (transform.position, uwd, out hit, joinDistanceUp)&&joinUp) {
 			transform.position = new Vector3 (hit.transform.position.x, hit.transform.position.y, hit.transform.position.z - blocSize);
-		}else if (Physics.Raycast (transform.position, dwd, out hit, 10)) {
+		}else if (Physics.Raycast (transform.position, dwd, out hit, joinDistanceDown)&&joinDown) {
 			transform.position = new Vector3 (hit.transform.position.x, hit.transform.position.y, hit.transform.position.z + blocSize);
 		}
 	}
---
+++ Unity_projects/Main_project/Assets/DungeonCreation/Scripts/PaletteBrick.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaletteBrick : MonoBehaviour {
+	public GameObject brick;
+	void OnMouseDown()
+	{
+		Instantiate (brick,transform, Quaternion.identity);
+	}
+}
